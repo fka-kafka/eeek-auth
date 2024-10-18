0edb92ebb0a427444e0d69b98b8939d928bb02b3
fka-kafka <kigenbrandon@gmail.com>
2024-10-18 15:33:38 +0300

    local build works

--- client/src/components/GoogleSignIn.tsx
+++ client/src/components/GoogleSignIn.tsx
@@ -16,13 +16,13 @@ const GoogleSignIn = ({ setSignedUp }: any) => {
     };
 
     const initializeGoogleSignIn = () => {
-      if (window.google && window.google.accounts) {
+      if (window.google && window.google.accounts && buttonRef.current) {
         window.google.accounts.id.initialize({
           client_id: import.meta.env.VITE_GSI_CLIENT_ID,
           callback: handleCredentialResponse,
         });
 
-        window.google.accounts.id.renderButton(buttonRef.current, {
+        window.google.accounts.id.renderButton(buttonRef.current!, {
           theme: "outline",
           size: "large",
           type: "icon",

b5508d071fe11c319f6e135f665509574dfed317
J. Brandon Kigen <126805003+fka-kafka@users.noreply.github.com>
2024-10-24 16:48:48 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.frontend_url,
+    allow_origins=[settings.frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

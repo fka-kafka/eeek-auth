a2bdd58541f29abd88ff2d283ba97e40bead4396
fka-kafka <kigenbrandon@gmail.com>
2025-03-26 21:58:32 +0300

    fix: fixed incorrect return type

--- client/src/modules/submitNewUser.ts
+++ client/src/modules/submitNewUser.ts
@@ -61,7 +61,7 @@ export async function userLogIn(username: string, password: string): Promise<Axi
   }
 }
 
-export async function userSSOLogIn(payload: string, provider: string): Promise<AxiosResponse<any, number>> {
+export async function userSSOLogIn(payload: string, provider: string): number {
   try {
     const response = await instance.post(`/login-sso/${provider}`, {
       content: payload,

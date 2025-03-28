8ac67ff6b030505e4336096c4bcf7cc181c84b43
fka-kafka <kigenbrandon@gmail.com>
2025-03-28 11:40:55 +0300

    fix: fixed environment dependent variables

--- app/redis_db.py
+++ app/redis_db.py
@@ -3,10 +3,16 @@ from config import get_settings
 
 settings = get_settings()
 
+
 def get_redis():
+
     return redis.Redis(
         host=settings.redis_host,
-        port=settings.redis_port,
+        port=int(settings.redis_port),
         password=settings.redis_password,
+        decode_responses=True
+    ) if settings.environment == 'development' else redis.Redis(
+        host=settings.redis_host,
+        port=int(settings.redis_port),
         decode_responses=True
     )
